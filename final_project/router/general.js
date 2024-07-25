c6bd21f5abe4ec3245b4446f79fe7ec4fe1c9c26
Christopher McKay <101884182+karotkriss@users.noreply.github.com>
2024-07-25 06:44:04 -0400

    add: customer login

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,89 +1,43 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
 let books = require("./booksdb.js");
-let isValid = require("./auth_users.js").isValid;
-let users = require("./auth_users.js").users;
-const public_users = express.Router();
+const regd_users = express.Router();
 
-const getBooks = () => {
-  return new Promise((resolve, reject) => {
-    resolve(books);
-  });
-};
+let users = [];
 
-const getByISBN = (isbn) => {
-  return new Promise((resolve, reject) => {
-    let isbnNum = parseInt(isbn);
-    if (books[isbnNum]) {
-      resolve(books[isbnNum]);
-    } else {
-      reject({ status: 404, message: `ISBN ${isbn} not found` });
-    }
-  });
-};
+const isValid = (username) => { //returns boolean
+  const userMatches = users.filter((user) => user.username === username);
+  return userMatches.length > 0;
+}
 
-public_users.post("/register", (req, res) => {
+const authenticatedUser = (username, password) => { //returns boolean
+  //write code to check if username and password match the one we have in records.
+  const matchingUsers = users.filter((user) => user.username === username && user.password === password);
+  return matchingUsers.length > 0;
+}
+
+//only registered users can login
+regd_users.post("/login", (req, res) => {
   //Write your code here
-  const { username, password } = req.body;
-  if (!username || !password) {
-    return res.status(400).json({ message: "Username and password are required" });
-  }
-  if (users.find((user) => user.username === username)) {
-    return res.status(409).json({ message: "Username already exists" });
-  }
-  users.push({ username, password });
-  return res.status(201).json({ message: "User registered successfully" });
-});
+  const username = req.body.username;
+  const password = req.body.password;
 
-// Get the book list available in the shop
-public_users.get('/', async function (req, res) {
-  try {
-    const bookList = await getBooks();
-    res.json(bookList);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Error retrieving book list" });
+  if (authenticatedUser(username, password)) {
+    let accessToken = jwt.sign({ data: password }, "access", { expiresIn: 3600 });
+    req.session.authorization = { accessToken, username };
+    return res.status(200).send("User successfully logged in");
+  }
+  else {
+    return res.status(208).json({ message: "Invalid username or password" });
   }
 });
 
-// Get book details based on ISBN
-public_users.get('/isbn/:isbn', function (req, res) {
+// Add a book review
+regd_users.put("/auth/review/:isbn", (req, res) => {
   //Write your code here
-  getByISBN(req.params.isbn)
-    .then(
-      result => res.send(result),
-      error => res.status(error.status).json({ message: error.message })
-    );
+  return res.status(300).json({ message: "Yet to be implemented" });
 });
 
-// Get book details based on author
-public_users.get('/author/:author', function (req, res) {
-  //Write your code here
-  const author = req.params.author;
-  getBooks()
-    .then((bookEntries) => Object.values(bookEntries))
-    .then((books) => books.filter((book) => book.author === author))
-    .then((filteredBooks) => res.send(filteredBooks));
-});
-
-// Get all books based on title
-public_users.get('/title/:title', function (req, res) {
-  //Write your code here
-  const title = req.params.title;
-  getBooks()
-    .then((bookEntries) => Object.values(bookEntries))
-    .then((books) => books.filter((book) => book.title === title))
-    .then((filteredBooks) => res.send(filteredBooks));
-});
-
-//  Get book review
-public_users.get('/review/:isbn', function (req, res) {
-  //Write your code here
-  const isbn = req.params.isbn;
-  getByISBN(req.params.isbn)
-    .then(
-      result => res.send(result.reviews),
-      error => res.status(error.status).json({ message: error.message })
-    );
-});
-
-module.exports.general = public_users;
+module.exports.authenticated = regd_users;
+module.exports.isValid = isValid;
+module.exports.users = users;
